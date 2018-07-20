987d3d3e1872b28d3532c59a26fdafaeeea6412c
Johan Larsson <johanlarsson@fullstackare.se>
2018-07-20 22:50:14 +0200

    Make buttons functional

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -15,21 +17,39 @@ public class Controller implements Initializable {
 
 
     @FXML
-    private Label label;
+    private Label paneUpLabel;
 
     @FXML
     private ImageView btnUp, btnDown, btnSettings, btnPower;
 
     @FXML
-    private AnchorPane paneUp, paneDown, paneSettings, paneStage;
+    private AnchorPane paneTop, paneUp, paneDown, paneSettings, paneStage;
 
     @FXML
     private void handleButtonAction(javafx.scene.input.MouseEvent event) {
         if ( event.getTarget() == btnUp) {
-            System.out.println("You clicked me");
-            label.setText("Hello World");
+            paneUp.setVisible(true);
+            paneDown.setVisible(false);
+            paneSettings.setVisible(false);
+        } else if ( event.getTarget() == btnDown) {
+            paneUp.setVisible(false);
+            paneDown.setVisible(true);
+            paneSettings.setVisible(false);
+        } else if ( event.getTarget() == btnSettings) {
+            paneUp.setVisible(false);
+            paneDown.setVisible(false);
+            paneSettings.setVisible(true);
+        } else if ( event.getTarget() == btnPower) {
+            if (!(paneUp.isVisible() || paneDown.isVisible() || paneSettings.isVisible())) {
+                Platform.exit();
+            } else {
+                paneUp.setVisible(false);
+                paneDown.setVisible(false);
+                paneSettings.setVisible(false);
+            }
         }
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Loading user data...");
